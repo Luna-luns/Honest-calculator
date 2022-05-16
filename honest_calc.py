a507aed8d10cd1860176ceaee07636abdbda4355
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-05-16 18:55:23 +0300

    Stage 3

--- honest_calc.py
+++ honest_calc.py
@@ -16,17 +16,39 @@ def calculate(_x: float, _y: float, _oper: str) -> float:
         return _x / _y
 
 
+def store_or_not() -> bool:
+    return input('Do you want to store the result? (y / n):') == 'y'
+
+
+def calculate_or_not() -> bool:
+    return input('Do you want to continue calculations? (y / n):') == 'y'
+
+
 print('Enter an equation')
+memory = 0
 while True:
     try:
         x, oper, y = input().split()
+
+        if x == 'M':
+            x = memory
+        if y == 'M':
+            y = memory
 
         float(x), float(y)
 
         if not check_operator(oper):
             print("Yes ... an interesting math operation. You've slept through all classes, haven't you?\nEnter an equation")
         else:
-            print(calculate(float(x), float(y), oper))
+            result = calculate(float(x), float(y), oper)
+            print(result)
+            answer_store = store_or_not()
+            answer_calculate = calculate_or_not()
+            if answer_store:
+                memory = result
+            if answer_calculate:
+                print('Enter an equation')
+                continue
             break
 
     except ValueError:
